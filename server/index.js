e969d635cedc5631e2b6ceeb0d6857c4065dcabf
Daniil Zaretskiy <zaretskiy03@gmail.com>
2023-05-15 02:40:26 +0300

    configured static imgs

--- server/index.js
+++ server/index.js
@@ -2,22 +2,25 @@ require('dotenv').config()
 const express = require('express')
 
 const models = require('./models/models')
-
-
 const router = require('./routes/index')
-
 const sequelize = require('./db')
-
 const cors = require('cors')
-
+const fileUpload = require('express-fileupload')
 const PORT = process.env.PORT || 5000
+const errorHandler = require('./middleware/ErrorHandlingMiddleware')
+const path = require('path')
+
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(fileUpload({}))
 app.use('/api', router)
+app.use(express.static(path.resolve(__dirname, 'static')))
 
+//Обработка ошибок, последний Middleware
+app.use(errorHandler)
 
 
 app.get('/', (req, res) => {
