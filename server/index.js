b9d8e461ff1432bfec09062d8669ae85e23ae61e
Daniil Zaretskiy <zaretskiy03@gmail.com>
2023-05-14 22:19:25 +0300

    setting corps

--- server/index.js
+++ server/index.js
@@ -2,13 +2,22 @@ require('dotenv').config()
 const express = require('express')
 
 const models = require('./models/models')
+
+
 const sequelize = require('./db')
+
+const cors = require('cors')
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
 
+app.use(cors())
+app.use(express.json())
 
+app.get('/', (req, res) => {
+    res.status(200).json({message: 'Working!'})
+})
 
 const start = async () => {
     try {
