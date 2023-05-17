b09a448773dc9e4c45b54925990a74c2d2b7923d
Daniil Zaretskiy <zaretskiy03@gmail.com>
2023-05-17 21:06:33 +0300

    add navbar

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,34 @@
+import React, { useContext } from 'react';
+import { Context } from '../index';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { NavLink } from 'react-router-dom';
+import { SHOP_ROUTE } from '../utils/const';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import { observer } from 'mobx-react-lite';
+
+const NavBar = observer( () => {
+    const {user} = useContext(Context)
+    return (
+        <div>
+        <Navbar bg="dark" variant="dark">
+        <Container>
+          <NavLink style={{color: 'white', fontSize:'20px', textDecorationLine: 'none'}} to={SHOP_ROUTE}>BuyDevice.ru</NavLink>
+          {user.isAuth ?
+          <Nav className="ml-auto">
+            <Button variant={'outline-light'} >Панель администратора</Button>
+            <Button className="ms-2" variant={'outline-light'} >Выйти</Button>
+          </Nav>
+          :
+          <Nav className="ml-auto">
+            <Button variant={'outline-light'} onClick={() => user.setIsAuth(true)}>Авторизация</Button>
+          </Nav>
+}
+        </Container>
+      </Navbar>
+        </div>
+    );
+});
+
+export default NavBar;
--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Container from 'react-bootstrap/esm/Container';
 
 const Auth = () => {
     return (
-        <div>
-            AUTH
-        </div>
+        <Container 
+            className='d-flex justify-content-center align-items-center'
+            style={{height: window.innerHeight - 54}}
+        >
+            AUTH 
+        </Container>
     );
 };
 
