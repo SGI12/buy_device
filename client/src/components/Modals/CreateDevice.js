9400784f082568c20b5b09dfb89bd1ace6a724f8
Daniil Zaretskiy <zaretskiy03@gmail.com>
2023-05-22 20:11:56 +0300

    added modals

---
+++ client/src/components/Modals/CreateDevice.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Button, Form, Modal } from 'react-bootstrap';
+
+const CreateDevice = ({show, onHide}) => {
+    return (
+    <Modal
+        show={show}
+        onHide={onHide}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Добавить новый тип:
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+              <Form.Control
+                  placeholder='Введите название типа'
+              />
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={onHide} variant='outline-danger'>Закрыть</Button>
+          <Button onClick={onHide} variant='outline-success'>Добавить</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+};
+
+export default CreateDevice;
--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Container } from 'react-bootstrap';
+import CreateType from '../components/Modals/CreateType';
+import CreateBrand from '../components/Modals/CreateBrand';
+import CreateDevice from '../components/Modals/CreateDevice';
 
 const Admin = () => {
+    const [brandVisible, setBrandVisible] = useState(false)
+    const [typeVisible, setTypeVisible] = useState(false)
+    const [deviceVisible, setDeviceVisible] = useState(false)
     return (
         <Container style={{backgroundColor: '#212529', borderRadius: '25px'}} className='d-flex flex-column mt-5'>
             <h1 style={{color: 'white', textDecorationLine: 'none'}} className='d-flex justify-content-center p-2'>Панель администратора</h1>
-            <Button variant='outline-light' className='m-4 p-2'>Добавить тип</Button>
-            <Button variant='outline-light' className='m-4 p-2'>Добавить бренд</Button>
-            <Button variant='outline-light' className='m-4 p-2'>Добавить устройство</Button>
+            <Button 
+            variant='outline-light' 
+            className='m-4 p-2'
+            onClick={() => setTypeVisible(true)}
+            
+            >Добавить тип</Button>
+            <Button 
+            variant='outline-light' 
+            className='m-4 p-2'
+            onClick={() => setBrandVisible(true)}
+            
+            >Добавить бренд</Button>
+            <Button 
+            variant='outline-light' 
+            className='m-4 p-2'
+            onClick={() => setDeviceVisible(true)}
+            
+            >Добавить устройство</Button>
+            <CreateType show={typeVisible} onHide={() => setTypeVisible(false)}/>
+            <CreateBrand show={brandVisible} onHide={() => setBrandVisible(false)}/>
+            <CreateDevice show={deviceVisible} onHide={() => setDeviceVisible(false)}/>
         </Container>
     );
 };
